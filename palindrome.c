8a31e277f0440b8d4cee8f9b4284710d508f566d
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-08-08 12:16:37 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+long long int n,tempvar,rev=0;
+scanf("%lld",&n);
+tempvar=n;
+while(n>0)
+{
+rev=rev*10+n%10;
+n/=10;
+}
+if(tempvar==n)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+
+
+}
